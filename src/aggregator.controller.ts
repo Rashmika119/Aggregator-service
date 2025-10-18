d3226d3e7f79dcb2a7902501029b189a37d7c3cb
Rashmika119 <rashmikanethsarni119@gmail.com>
2025-10-18 23:53:50 +0530

    added logs for aggragtor controller

--- src/aggregator.controller.ts
+++ src/aggregator.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { AggregatorService } from './aggregator.service';
 
 
@@ -6,9 +6,9 @@ import { AggregatorService } from './aggregator.service';
 
 @Controller()
 export class AggregatorController {
-
+  private readonly logger = new Logger(AggregatorController.name);
   constructor(private readonly aggregatorService: AggregatorService,
-   
+
   ) { }
 
   //-----------------------------version 1-------------------
@@ -20,7 +20,9 @@ export class AggregatorController {
     @Query('endDestination') endDestination: string,
     @Query('departTime') departTime: string,
   ) {
-
+    this.logger.log(
+      `GET /v1/trips/search called with startDestination=${startDestination}, endDestination=${endDestination}, departTime=${departTime}`,
+    );
     return this.aggregatorService.getFlightAndHotelInfo(startDestination, endDestination, departTime);
   }
 
@@ -34,7 +36,9 @@ export class AggregatorController {
     @Query('endDestination') endDestination: string,
     @Query('departTime') departTime: string,
   ) {
-
+    this.logger.log(
+      `GET /v2/trips/search called with startDestination=${startDestination}, endDestination=${endDestination}, departTime=${departTime}`,
+    );
     return this.aggregatorService.getInfoWithWeather(startDestination, endDestination, departTime);
   }
 
@@ -45,14 +49,16 @@ export class AggregatorController {
   //chain pattern
 
   @Get('v1/trips/cheapest_route')
- async getChepestRouteWithHotels(
-        @Query('startDestination') startDestination:string,
-        @Query('endDestination') endDestination:string,
-        @Query('departTime') departTime:string,
-  ){
-       
-        return this.aggregatorService.getBudgetRoute(startDestination,endDestination,departTime);
-      }
+  async getChepestRouteWithHotels(
+    @Query('startDestination') startDestination: string,
+    @Query('endDestination') endDestination: string,
+    @Query('departTime') departTime: string,
+  ) {
+    this.logger.log(
+      `GET /v1/trips/cheapest_route called with startDestination=${startDestination}, endDestination=${endDestination}, departTime=${departTime}`,
+    );
+    return this.aggregatorService.getBudgetRoute(startDestination, endDestination, departTime);
+  }
 
   //---------------------------version 1--------------------------
   //flight + hotels + events
@@ -61,15 +67,18 @@ export class AggregatorController {
 
   @Get('v1/trips/contextual')
   async getEventsWithHotels(
-    @Query('startDestination') starttDestination:string,
-    @Query('endDestination') endDestination:string,
+    @Query('startDestination') startDestination: string,
+    @Query('endDestination') endDestination: string,
     //when send data through a url everything treated as astring.no matter the intended type
-    @Query('departTime') departTime:string
+    @Query('departTime') departTime: string
 
-  ){
-    
+  ) {
+    this.logger.log(
+      `GET /v1/trips/contextual called with startDestination=${startDestination}, endDestination=${endDestination}, departTime=${departTime}`,
+    );
+
     //getEventDest method expect Date object
-    return this.aggregatorService.getEventInDestination(starttDestination,endDestination,departTime);
+    return this.aggregatorService.getEventInDestination(startDestination, endDestination, departTime);
     ;
   }
 }
