b615a6836750e16f4cb45fee8fc63b5f6432db72
Rashmika119 <rashmikanethsarni119@gmail.com>
2025-10-19 22:02:54 +0530

    added validation pipe and import to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './aggregator.module';
+import { createValidationPipe } from './pipe/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(createValidationPipe());
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
